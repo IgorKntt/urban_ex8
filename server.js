99d0ee12ff8347af8fcdb7e175cdf2aae83bc77d
IgorKntt <igor.gorskin@hotmail.com>
2024-10-18 10:52:38 +0500

    Created project structure and all files

---
+++ server.js
@@ -0,0 +1,9 @@
+const math = require("./math.js");
+
+console.log(`Выполнение функции "add (10, 15)" из подклюенного модуля math.js: ${math.add(10, 15)}`);
+
+console.log(`Выполнение функции "subtract(25, 10)" из подклюенного модуля math.js: ${math.subtract(25, 10)}`);
+
+console.log(`Выполнение функции "multiply(5, 2))" из подклюенного модуля math.js: ${math.multiply(5, 2)}`);
+
+console.log(`Выполнение функции "divide(40, 5) из подклюенного модуля math.js: ${math.divide(40, 5)}`);
